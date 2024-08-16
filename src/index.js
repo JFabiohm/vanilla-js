9c07e7200c757501f644930342703b6555e6b2bc
JFabiohm <kawaighm@gmail.com>
2024-08-16 15:27:02 -0600

    trabajo 5 terminado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 // Inserte el código aquí
 
+class calcu {
+    constructor(numero, numero2) {
+        this.numero=numero;
+        this.numero2=numero2;
+    }
+
+    suma(numero,numero2){
+       return this.numero+this.numero2;
+    }
+    resta(){
+        return this.numero-this.numero2;
+    }
+    multiplica(){
+        return this.numero*this.numero2;
+    }
+    divide(){
+        return this.numero/this.numero2;
+    }
+}
---
+++ src/index2.js
@@ -0,0 +1,6 @@
+import {calcu} from "./exportar";
+let operacion=new calcu(2,4);
+console.log(operacion.multiplica());
+console.log(operacion.suma());
+console.log(operacion.resta());
+console.log(operacion.divide())
